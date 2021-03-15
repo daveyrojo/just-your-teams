d9569084718e128dec3e499a659576ddae5d7a8e
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-15 14:20:54 -0400

    created user mode for login, about me model for profile

---
+++ models/Aboutme.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+//may need to make a models for the sport, league, and team so user can have more than one team, league and sport to follow
+//or may need just a follow model that contains the the sports, leagues and teams, etc. need to review model structure
+
+class Aboutme extends Model {}
+
+Aboutme.init(
+  {
+    sport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    league: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    team: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    about_me: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = AboutMe;
