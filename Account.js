b2ce018fe86918c71d42abccef9829f6751b292f
James VanderKam <jamesvanderkam@verizon.net>
2021-03-15 13:51:20 -0400

    need to update with main

---
+++ Account.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Account extends Model {}
+
+Account.init(
+    {
+        // define columns
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+
+    },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'account',
+  }
+);
+
+module.exports = Account;
