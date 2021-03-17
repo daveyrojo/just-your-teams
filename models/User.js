2e853c37e09254734bfb19f5de6610e75c1ced84
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-17 11:55:52 -0400

    updated user model

--- models/User.js
+++ models/User.js
@@ -43,7 +43,6 @@ User.init(
   },
   {
      hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
