dae8a0fc93c23cc8628f7ab7659c57434e44e706
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-17 13:50:42 -0400

    updated pack.json

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+
 const userRoutes = require('./user-routes');
 const selectedRoutes = require('./selected-routes');
 const { route } = require("..");
 
-route.use('/uer', userRoutes);
+route.use('/user', userRoutes);
 route.use('/selected', selectedRoutes);
 
 module.exports = router;
