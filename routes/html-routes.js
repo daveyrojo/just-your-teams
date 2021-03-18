3578ced6d0057a150fb64e10d52f9bea81906fb6
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-18 13:26:24 -0400

    got link to create account to work

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 //every route needs to go ahead of app.get('*') or else it /// won't work
 
-router.get("/create", (req, res) => {
- res.sendFile(path.join(__dirname, "..views/create-acc.html"));   
+router.get("/create-acc", (req, res) => {
+ res.sendFile(path.join(__dirname, "../views/create-acc.html"));   
 });
 
 router.get("/user", (req, res) => {
-  res.sendFile(path.join(__dirname, "..views/userpage.html"));
+  res.sendFile(path.join(__dirname, "../views/userpage.html"));
 });
 
 router.get("*", (req, res) => {
