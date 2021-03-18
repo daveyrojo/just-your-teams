ece4dcb82b3871d9b0fe3d23f0c78e85edc29e9f
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-18 13:09:56 -0400

    added routes for userpage and create account page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,15 @@ const path = require("path");
 const router = require("express").Router();
 
 //every route needs to go ahead of app.get('*') or else it /// won't work
+
+router.get("/create", (req, res) => {
+ res.sendFile(path.join(__dirname, "..views/create-acc.html"));   
+});
+
+router.get("/user", (req, res) => {
+  res.sendFile(path.join(__dirname, "..views/userpage.html"));
+});
+
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/landing.html"));
 });
