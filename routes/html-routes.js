6430b9e5c3556cb801b64fde654340857646bb27
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-18 13:02:22 -0400

    fixed landing page issue

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,9 @@
 const path = require("path");
+const router = require("express").Router();
 
 //every route needs to go ahead of app.get('*') or else it /// won't work
-module.exports = (app) => {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../html/index.html')
-        );
-    });
-};
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/landing.html"));
+});
+
+module.exports = router;
